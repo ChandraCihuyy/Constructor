492d51e8d9b7fa7f2ea21a7958ddb00a6a2b7450
ChandraCihuyy <156741359+ChandraCihuyy@users.noreply.github.com>
2024-01-30 14:13:27 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package user;
+public class Main {
+    public static void main(String[] args){
+        User Vigo = new User();
+         
+        Vigo.setUsername("Vigo");
+        Vigo.setPassword("alamak");
+
+
+System.out.println("Username: " + Vigo.getUsername());
+System.out.println("Password: " + Vigo.getPassword());  
+        
+    }
+}
---
+++ User.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package user;
+public class User {
+    String username;
+    String password;
+    
+    public User(){
+        System.out.println("eksekusi method constructor...");
+  }
+
+    
+
+    // ini method setter
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    // ini method getter
+    public String getUsername(){
+        return this.username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+}
+
+
+
+
+
+
+    
+
